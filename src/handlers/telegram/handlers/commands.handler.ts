93672d0574a084ff2af09751c9cd59bdf209c051
yuriipidlisnyi <yuriypidlisnyi2020@gmail.com>
2026-05-24 12:53:25 +0200

    tmp: error command for sentry testing

--- src/handlers/telegram/handlers/commands.handler.ts
+++ src/handlers/telegram/handlers/commands.handler.ts
@@ -45,6 +45,11 @@ export function createCommandHandler(
 		);
 	});
 
+	// TODO: Drop me after testing
+	composer.command("error", async (ctx) => {
+		throw new Error("Error on composer commmand for Sentry test");
+	});
+
 	composer.command("macros", async (ctx) => {
 		const buttonRow = TIMEFRAMES_FOR_ANALYSIS.map(([label, data]) =>
 			InlineKeyboard.text(label, data),
